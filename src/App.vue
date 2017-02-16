205237ead209182787e3b2b3af8af8c6cf9a45aa
gejiaxing <hzgjx@trairanchina.com>
2017-02-16 20:56:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <!--<div id="app">-->
+  <div id="fullpage">
+    <div class="section pg1">
+      <hello></hello>
+    </div>
+    <div class="section pg2"></div>
+    <div class="section">
+      <div class="slide">slide</div>
+      <div class="slide">slide</div>
+      <div class="slide">slide</div>
+    </div>
+    <div class="section pg4">
+      <div id="bottom_mobile"><img src="../static/img/line_01.jpg"><span>15658132456</span></div>
+      <div id="bottom_qq"><img src="../static/img/line_02.jpg"><span>617405347</span></div>
+    </div>
+  </div>
+  <!--</div>-->
+</template>
+
+<script>
+  import Hello from './components/Hello'
+  import $ from 'jquery'
+  import jqueryEasing from 'jquery.easing'
+  import fullPage from 'fullpage.js'
+
+  $(document).ready(function () {
+    $('#fullpage').fullpage({
+      sectionsColor: ['black', 'white', 'gray', '#1e1b1e'],
+      controlArrows: false,
+      resize: true,
+      scrollingSpeed: 1000,
+      anchors: ['page1', 'page2', 'page3', 'page4'],
+      recordHistory: false,
+//      menu: '#nav',
+      navigation: true,
+      navigationPosition: 'right',
+      slidesNavigation: true,
+      navigationColor: '#fff',
+      slidesNavPosition: 'bottom'
+    });
+  });
+
+  setInterval(function () {
+    $.fn.fullpage.moveSlideRight();
+  }, 10000);
+
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    }
+  }
+</script>
+
+<style>
+  @import "assets/lib/css/jquery.fullPage.min.css";
+
+  #fp-nav ul li a span, .fp-slidesNav ul li a span {
+    background-color: white;
+  }
+
+  .section {
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+
+  div .pg1 {
+    background-image: url(../static/img/pg_01.png);
+    /*background-size: cover;*/
+  }
+
+  div .pg2 {
+    background-image: url(../static/img/pg_02.png);
+
+  }
+
+  div .pg4 {
+    background-image: url(../static/img/pg_04.png);
+    background-size: auto;
+    background-position: top;
+  }
+  #bottom_mobile{
+    position: absolute;
+    top: 85%;
+    left: 25%;
+    height: 100%;
+    width: 100%;
+  }
+  #bottom_qq{
+    position: absolute;
+    top: 85%;
+    left: 55%;
+    height: 100%;
+    width: 100%;
+  }
+  .pg4 div span{
+    color: white;
+    font-size: 2.5vw;
+    display: inline-block;
+    position: absolute;
+    margin: 0 auto;
+    padding-left: 1%;
+  }
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,108 @@
+<template>
+  <div id="top-bar">
+    <div id="logo">
+      <img id="nav-logo" src="../../static/img/logo.png">
+    </div>
+    <div id="nav">
+      <ul>
+        <li data-menuanchor="page1" class="active"><a href="#page1">首页</a></li>
+        <li data-menuanchor="page2"><a href="#page2">团队名介绍</a></li>
+        <li data-menuanchor="page3"><a href="#page3">优秀项目</a></li>
+        <li data-menuanchor="page4"><a href="#page4">成功案例</a></li>
+      </ul>
+    </div>
+    <div id="tel">
+      <img src="../../static/img/tel_logo.png">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  #top-bar {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    z-index: 999;
+    padding: 0;
+    height: 4.25rem;
+    background: white;
+    /*filter:alpha(Opacity=60);-moz-opacity:0.6;opacity: 0.6;*/
+  }
+
+  #logo {
+    float: left;
+    /*width: 10%;*/
+    padding-left: 17%;
+  }
+
+  #logo img {
+    width: 75%;
+    height: 75%;
+    position: relative;
+    margin-top: 8%;
+  }
+
+  #nav {
+    width: 40%;
+    position: absolute;
+    top: 0;
+    left: 30%;
+  }
+
+  #tel {
+    float: right;
+    padding-right: 32%;
+    /*width: 10%;*/
+    /*height: 75%;*/
+    /*padding-top: 10%;*/
+  }
+
+  #tel img {
+    width: 75%;
+    height: 75%;
+    position: relative;
+    margin-top: 13%;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  #nav ul {
+    /*width:100%;*/
+    /*height:35px;*/
+    top: 0;
+    display: inline;
+  }
+
+  #nav ul li {
+    width: 20%;
+    height: 100%;
+    float: left;
+    list-style: none;
+    text-align: center;
+  }
+
+  #nav ul li a {
+    display: block;
+    /*text-indent: 30px;*/
+    height: 100%;
+    line-height: 4.25rem;
+    color: #000;
+    font-size: 1vw;
+    font-family: "Helvetica", "Tahoma", "Arial", "PingFang SC", "Microsoft JhengHei", "SimSun", "sans-serif";
+    font-weight: 500;
+  }
+
+  #nav ul li a:hover {
+    color: #ff0000;
+  }
+</style>
